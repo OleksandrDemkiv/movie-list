2f0678953f65e737a26f916ef0aee6de75902a11
OleksandrDemkiv <s00251154@atu.ie>
2025-02-12 11:06:13 +0000

    week4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { MovieService } from './movie.service';
+
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { ListMoviesComponent } from './list-movies/list-movies.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, AddMovieComponent, ListMoviesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'movie-list';
 
-  movies: any[] = []
-
-  constructor(private MovieService: MovieService) {}
-
-  ngOnInit() {
-    this.movies = this.MovieService.getMovies();
-    console.log(this.movies);
-  }
 }
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -8,13 +8,17 @@ export class MovieService {
   constructor() { }
 
   moviesList = [
-    { id: 1, title: 'The Shawshank Redemption', year: 1994, director: 'Frank Darabont' },
-    { id: 2, title: 'The Godfather', year: 1972, director: 'Francis Ford Coppola' },
-    { id: 3, title: 'The Dark Knight', year: 2008, director: 'Christopher Nolan' },
-    { id: 4, title: 'The Lord of the Rings: The Return of the King', year: 2003, director: 'Peter Jackson' },
+    { title: 'The Shawshank Redemption', year: '1994', director: 'Frank Darabont' },
+    { title: 'The Godfather', year: '1972', director: 'Francis Ford Coppola' },
+    { title: 'The Dark Knight', year: '2008', director: 'Christopher Nolan' },
+    { title: 'The Lord of the Rings: The Return of the King', year: '2003', director: 'Peter Jackson' },
   ]
 
   getMovies() {
     return this.moviesList;
+  }
+
+  addMovie(movietitle:string, moviedirector:string, movieyear:string) {
+    this.moviesList.push({title:movietitle, year:movieyear, director:moviedirector})
   }
 }
